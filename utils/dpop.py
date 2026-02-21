e4b65320c48119b7de22c08630498ea289ee4ade
arionvalen8 <valentingr080@gmail.com>
2026-02-21 10:20:32 +0100

    change to webkit

--- utils/dpop.py
+++ utils/dpop.py
@@ -5,7 +5,7 @@ def get_dpop():
     search_url = "https://jp.mercari.com/search?keyword=%20%E3%82%A4%E3%83%8A%E3%82%BA%E3%83%9E%E3%82%A4%E3%83%AC%E3%83%96%E3%83%B3&order=desc&sort=created_time"
 
     with sync_playwright() as p:
-        browser = p.chromium.launch(headless=True)
+        browser = p.webkit.launch(headless=True)
         page = browser.new_page()
 
         with page.expect_request(lambda r: target in r.url, timeout=100000) as req_info:
